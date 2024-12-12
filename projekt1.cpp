a05e57d027a1b2409ee1b25dfb18985bd031d2b3
LucasGTX <lukasgtx71@gmail.com>
2024-12-12 12:55:12 +0100

    Dodano test liczby pierwszej

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,11 +2,35 @@
 
 using namespace std;
 
+bool CzyPierwsza(int a) {
+    if (a == 0 || a == 1) {
+        return false;
+    }
+
+    if (a == 2) {
+        return true;
+    }
+
+    if (a % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 3; i <= (a/2); i += 2) {
+        if (a % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
+
     int wyjscie;
+
     do {
         cout << endl;
         cout << endl;
